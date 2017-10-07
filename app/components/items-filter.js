06672cbaaf9ce72b9fe040729b9ae70ed6c4d719
Michał Czyż <m.czyz@selleo.com>
2017-10-07 13:01:08 +0200

    debounce search

--- app/components/items-filter.js
+++ app/components/items-filter.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    termChange() {
+      Ember.run.debounce(this, this.updateQuery, 500);
+    }
+  },
+  updateQuery() {
+    this.get('onChange')(this.get('searchTerm'));
+  },   
 });
