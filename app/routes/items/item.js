529439bc5eff90e9bab273e7c81708d1c1f7eb87
Michał Czyż <m.czyz@selleo.com>
2017-10-07 16:21:08 +0200

    item route & controller

--- app/routes/items/item.js
+++ app/routes/items/item.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  db: Ember.inject.service(),
+  model(params) {
+    return this.get('db').find('items', params.item_id);
+  }
 });
--- app/services/db.js
+++ app/services/db.js
@@ -6,6 +6,9 @@ export default Ember.Service.extend({
   all(collection) {
    return this[collection]
   },
+  find(collection, id) {
+   return this[collection].find(el => el.id == id)
+  },
   add(collection, element) {
    this[collection].pushObject(element);
   },
@@ -13,24 +16,28 @@ export default Ember.Service.extend({
   categories: uniq('allCategories'),
   items: [
     {
+      id: 1,
       name: 'Unibike Xenon Bike',
       category: 'Sports & Outdoors',
       imageUrl: 'http://tachyons.io/img/avatar-mrmrs.jpg',
       since: '2014-08-30',
     },
     {
+      id: 2,
       name: 'Sony XZ Phone',
       category: 'Personal Electronic',
       imageUrl: 'http://tachyons.io/img/avatar-jxnblk.jpg',
       since: '2017-06-20',
     },
     {
+      id: 3,
       name: 'Wash Machine',
       category: 'Appliances',
       imageUrl: 'http://tachyons.io/img/avatar-jasonli.jpg',
       since: '2010-04-01',
     },
     {
+      id: 4,
       name: 'Dodge Viper',
       category: 'Cars',
       imageUrl:
