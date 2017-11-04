c041f7199b3448e4d540d6c22d8aa2170295588c
Michał Czyż <m.czyz@selleo.com>
2017-11-04 15:14:00 +0100

    cleanup after db:service

--- app/routes/items.js
+++ app/routes/items.js
@@ -1,16 +1,10 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  db: service(),
   queryParams: {
     query: {
       replace: true
     }
-  },
-  beforeModel() {
-    this.get('db').all('items').forEach( item => this.store.createRecord('item', item).save());
-    this.get('db').all('events').forEach( event => this.store.createRecord('event', event).save());
   },
   model() {
     return this.store.peekAll('item');
