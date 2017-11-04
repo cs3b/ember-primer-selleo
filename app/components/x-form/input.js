f1e162539b218ac931c433cc40dbc3478fc8d28e
Michał Czyż <m.czyz@selleo.com>
2017-11-04 12:04:14 +0100

    x-form/input
    * component block
    * defaults

--- app/components/x-form/input.js
+++ app/components/x-form/input.js
@@ -1,6 +1,10 @@
 import Component from '@ember/component';
+import { get, computed } from '@ember/object'
 
 const Input = Component.extend({
+  placeholder: computed('label', function() {
+    return `Please enter the ${get(this, 'label').toLowerCase()}`
+  })
 });
 
 Input.reopenClass({
