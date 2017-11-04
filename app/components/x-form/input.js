3f673dd8b6b7d0732ed525159cb04b123c214026
Michał Czyż <m.czyz@selleo.com>
2017-11-04 12:43:53 +0100

    x-form/date input

--- app/components/x-form/input.js
+++ app/components/x-form/input.js
@@ -2,6 +2,16 @@ import Component from '@ember/component';
 import { get, computed } from '@ember/object'
 
 const Input = Component.extend({
+  inputType: 'text',
+  inputWidth: 100,
+  inputClasses: computed('inputWidth', function() {
+    let base = 'input-reset ba b--black-20 pa2 mb2 db';
+    let inputWidth = get(this, 'inputWidth');
+    if (inputWidth) {
+      base += ` w-${inputWidth}`
+    }
+    return base;
+  }),
   placeholder: computed('label', function() {
     return `Please enter the ${get(this, 'label').toLowerCase()}`
   })
