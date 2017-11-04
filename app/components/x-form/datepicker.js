ed7d051ea066dca463f05ae0f5491b69da0e97a5
Michał Czyż <m.czyz@selleo.com>
2017-11-04 14:10:10 +0100

    x-form/datepicker - integration with 3rd party js library

---
+++ app/components/x-form/datepicker.js
@@ -0,0 +1,22 @@
+import Date from 'track-items/components/x-form/date';
+import layout from 'track-items/templates/components/x-form/input';
+import $ from 'jquery';
+import { get, set } from '@ember/object';
+
+export default Date.extend({
+  layout,
+  inputType: 'text',
+  
+  didInsertElement() {
+    this._super(...arguments);
+
+    set(this, 'pikaday', new Pikaday({
+      field: $('input', this.element)[0], 
+      format: 'YYYY-MM-DD'}))
+  },
+  
+  willDestroyElement() {
+    this._super(...arguments);
+    get(this, 'pikaday').destroy();
+  }
+});
